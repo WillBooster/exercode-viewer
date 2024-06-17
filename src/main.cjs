3c88ad43a27817f63a2c6174d187044f8c50ea16
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-06-17 20:21:27 +0900

    fix: wbfy this repo

--- src/main.cjs
+++ src/main.cjs
@@ -3,15 +3,15 @@ const fs = require('node:fs');
 const os = require('node:os');
 const path = require('node:path');
 
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { BrowserWindow, app, ipcMain } = require('electron');
 const fflate = require('fflate');
 
 const createWindow = () => {
   const { screen } = require('electron');
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const { height, width } = screen.getPrimaryDisplay().workAreaSize;
   const mainWindow = new BrowserWindow({
-    width: width,
-    height: height,
+    width,
+    height,
     webPreferences: {
       preload: path.join(__dirname, 'preload.cjs'),
       devTools: false,
@@ -22,36 +22,36 @@ const createWindow = () => {
 
   mainWindow.webContents.on('did-finish-load', () => {
     void mainWindow.webContents.executeJavaScript(`
-    setInterval(function() {
-      const chakraStackElem = document.querySelector('.chakra-stack');
-      if (chakraStackElem && !document.querySelector('#back')) {
-        const backButton = document.createElement('button');
-        backButton.textContent = 'Back';
-        backButton.id = 'back';
-        backButton.onclick = () => window.history.back();
-        const firstChild = chakraStackElem.firstChild;
-        chakraStackElem.insertBefore(backButton, firstChild);
+setInterval(function() {
+  const chakraStackElem = document.querySelector('.chakra-stack');
+  if (chakraStackElem && !document.querySelector('#back')) {
+    const backButton = document.createElement('button');
+    backButton.textContent = 'Back';
+    backButton.id = 'back';
+    backButton.onclick = () => window.history.back();
+    const firstChild = chakraStackElem.firstChild;
+    chakraStackElem.insertBefore(backButton, firstChild);
 
-        const forwardButton = document.createElement('button');
-        forwardButton.textContent = 'Forward';
-        forwardButton.onclick = () => window.history.forward();
-        chakraStackElem.insertBefore(forwardButton, firstChild);
-      }
+    const forwardButton = document.createElement('button');
+    forwardButton.textContent = 'Forward';
+    forwardButton.onclick = () => window.history.forward();
+    chakraStackElem.insertBefore(forwardButton, firstChild);
+  }
 
-      const downloadElem = document.querySelector('#download');
-      const executionCommandElem = document.querySelector('#execution');
-      if (downloadElem && executionCommandElem) {
-        const zipUrl = downloadElem.href;
-        downloadElem.onclick = async () => {
-          const response = await fetch(zipUrl);
-          const buffer = await response.arrayBuffer();
-          window.electron.execute(zipUrl, buffer, executionCommandElem.textContent);
-        };
-        downloadElem.removeAttribute('href');
-        downloadElem.id = 'execute';
-        downloadElem.textContent = 'Execute on Your PC';
-      }
-    }, 100);
+  const downloadElem = document.querySelector('#download');
+  const executionCommandElem = document.querySelector('#execution');
+  if (downloadElem && executionCommandElem) {
+    const zipUrl = downloadElem.href;
+    downloadElem.onclick = async () => {
+      const response = await fetch(zipUrl);
+      const buffer = await response.arrayBuffer();
+      window.electron.execute(zipUrl, buffer, executionCommandElem.textContent);
+    };
+    downloadElem.removeAttribute('href');
+    downloadElem.id = 'execute';
+    downloadElem.textContent = 'Execute on Your PC';
+  }
+}, 100);
     `);
   });
 };
@@ -76,18 +76,29 @@ app.whenReady().then(() => {
     let args;
     executionCommand = executionCommand.split(' && ').slice(2).join(' && ');
     switch (os.platform()) {
-      case 'win32': // Windows
+      case 'win32': {
+        // Windows
         command = 'start';
         args = ['cmd.exe', '/K', `cd ${dirPath} && ${executionCommand}`];
         break;
-      case 'darwin': // macOS
+      }
+      case 'darwin': {
+        // macOS
         command = 'osascript';
-        args = ['-e', `tell app "Terminal" to do script "cd ${dirPath} && ${executionCommand.replaceAll('"', '\'')}"`, '-e', 'tell app "Terminal" to activate'];
+        args = [
+          '-e',
+          `tell app "Terminal" to do script "cd ${dirPath} && ${executionCommand.replaceAll('"', "'")}"`,
+          '-e',
+          'tell app "Terminal" to activate',
+        ];
         break;
-      default: // Linux
+      }
+      default: {
+        // Linux
         command = 'gnome-terminal';
         args = ['--', 'bash', '-c', `cd ${dirPath} && ${executionCommand}`];
         break;
+      }
     }
     child_process.spawn(command, args, { shell: false });
   });
